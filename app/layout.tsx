24189e126d1d8f423d163188f92565f400358de8
Jayson Baloyi <jysnbly7@gmail.com>
2025-11-24 21:16:54 +0200

    sidebar err

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@
 
 
 
-// import { Sidebar } from '@/components/Layout/Sidebar';
+import { Sidebar } from '@/components/Layout/Sidebar';
 import './globals.css';
 import { Footer } from '@/components/Layout/Footer';
 import ClientShell from './ClientShell';
@@ -15,6 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className="scroll-smooth">
       <body className="antialiased md:pl-24 selection:bg-amber-900 selection:text-white overflow-x-hidden bg-[#050505] text-[#e5e5e5] font-sans">
+        <Sidebar />
         <ClientShell>
           <div className="pt-20 md:pt-0">
             {children}
